e2779962d85247812081595345f56fac6a68337e
Germain Loret <germain.loret@gmail.com>
2018-05-12 14:16:22 +0200

    added loader

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -3,6 +3,7 @@ import { contactForm } from './components/contactForm';
 import { changeBannerPicture } from './components/changeBannerPicture';
 import { populateCarousel, triggerClickOnProduct, fetchUrlId } from './components/product_carousel';
 import { getData } from './services/prismic_api';
+import { hideLoader } from './components/loader';
 
 mobileMenu();
 contactForm();
@@ -12,6 +13,7 @@ changeBannerPicture();
 if (document.querySelector("#product-carousel")) {
   getData().then(results => {
     populateCarousel(results).then(() => {
+      hideLoader();
       triggerClickOnProduct();
       fetchUrlId();
     });
