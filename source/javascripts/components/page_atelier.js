8dc8466766a46e55139f92393933f626fed9d46e
Germain Loret <germain.loret@gmail.com>
2018-05-25 17:12:55 +0200

    start js

--- source/javascripts/components/page_atelier.js
+++ source/javascripts/components/page_atelier.js
@@ -1,8 +1,25 @@
+function updateDot(dotId) {
+  const dots = document.querySelectorAll('#dots .dot');
+  dots.forEach((dot) => {
+    dot.classList.add('empty');
+  })
+  document.querySelector(`[data-dot-id="${dotId}"]`).classList.remove('empty');
+}
+
+function updateImage(imageId) {
+  const images = document.querySelectorAll('#atelier-pictures img');
+  images.forEach((image) => {
+    image.classList.add('hidden');
+  })
+  document.querySelector(`[data-image-id="${imageId}"]`).classList.remove('hidden');
+}
+
 function clickOnDot() {
   const dots = document.querySelectorAll('#dots .dot');
   dots.forEach((dot) => {
     dot.addEventListener('click', (event) => {
-      console.log(event.target);
+      updateImage(event.target.dataset.dotId);
+      updateDot(event.target.dataset.dotId);
     })
   })
 }
