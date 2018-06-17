d5d530874b2594e1caaeb6196aca5a088cbc1e2a
germainloret <germain.loret@gmail.com>
2018-06-17 18:17:30 +0200

    Add function to populate 'points de vente' page

---
+++ source/javascripts/components/page_points_de_vente.js
@@ -0,0 +1,39 @@
+function populateSalon(container, salons) {
+  if (salons.length !== 0) {
+    container.insertAdjacentHTML('beforeend', `<div class="row"><div class="col-xs-offset-2 col-xs-8 salons"><h2 class="uppercase">Salons</h2></div></div>`);
+    salons.forEach((salon) => {
+      let content;
+      if (salon.salon_link.url) {
+        content = `<p><a target="_blank" href="${salon.salon_link.url}">${salon.salon_name}</a></p>`
+      } else {
+        content = `<p>${salon.salon_name}</p>`
+      }
+      container.querySelector('.salons')
+        .insertAdjacentHTML('beforeend', content);
+    })
+  }
+}
+
+function populateBoutique(container, boutiques) {
+  if (boutiques.length !== 0) {
+    container.insertAdjacentHTML('beforeend', `<div class="row"><div class="col-xs-offset-2 col-xs-8 boutiques"><h2 class="uppercase">Boutiques</h2></div></div>`);
+    boutiques.forEach((boutique) => {
+      let content;
+      if (boutique.boutique_link.url) {
+        content = `<p><a target="_blank" href="${boutique.boutique_link.url}">${boutique.boutique_name}</a></p>`
+      } else {
+        content = `<p>${boutique.boutique_name}</p>`
+      }
+      container.querySelector('.boutiques')
+        .insertAdjacentHTML('beforeend', content);
+    })
+  }
+}
+
+function populatePointsVente(data) {
+  const container = document.querySelector("#salons_boutiques");
+  populateSalon(container, data[0].data.salons)
+  populateBoutique(container, data[0].data.boutiques)
+}
+
+export default populatePointsVente;
--- source/javascripts/services/prismic_api.js
+++ source/javascripts/services/prismic_api.js
@@ -1,5 +1,5 @@
 function getData() {
-  const type = document.querySelector('#product-carousel, #container-editions').dataset.productType;
+  const type = document.querySelector('#product-carousel, #container-editions, #salons_boutiques').dataset.productType;
   const Prismic = require('prismic-javascript');
   const apiEndpoint = "https://mm-millimetrefr.prismic.io/api/v2";
   const apiQuery = Prismic.Predicates.at('document.type', type);
